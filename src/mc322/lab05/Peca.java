c16038ba91c6c2caa37efe5b780aeba7abdc9f8d
Rakiakagi <henriqueakiraakagi@gmail.com>
2021-04-30 16:07:24 -0300

    no message

--- src/mc322/lab05/Peca.java
+++ src/mc322/lab05/Peca.java
@@ -27,52 +27,47 @@ Coordenada coordenada = new Coordenada("a1");
     	Sudeste = pc;
     }
     
-    public boolean TestaPeca(Coordenada Ctarget) {
-    	if (P == 'p') {
+    public boolean TestaPeca(char TipoSource, Coordenada Ctarget) {
+    	if (TipoSource == 'p') {
     		if (Sudoeste != null) {
-	        	if (((Ctarget.linha == coordenada.linha+1)&&(Ctarget.coluna == coordenada.coluna-1)))
+	        	if (((Ctarget.linha == Sudoeste.coordenada.linha)&&(Ctarget.coluna == Sudoeste.coordenada.coluna)))
 	        		if (Sudoeste.P == '-')
 	        			return true;
 	        	if (Sudoeste.Sudoeste != null)
-	        		if (((Ctarget.linha == coordenada.linha+2)&&(Ctarget.coluna == coordenada.coluna-2)))
-	        			if (Sudoeste.P == 'b' || Sudoeste.P == 'B')
+	        		if (((Ctarget.linha == Sudoeste.Sudoeste.coordenada.linha)&&(Ctarget.coluna == Sudoeste.Sudoeste.coordenada.coluna)))
+	        			if ((Sudoeste.P == 'b' || Sudoeste.P == 'B') && (Sudoeste.Sudoeste.P == '-'))
 	        				return true;
     		}
     		else if (Sudeste != null) {
-	    		if (((Ctarget.linha == coordenada.linha+1)&&(Ctarget.coluna == coordenada.coluna+1)))
+	    		if (((Ctarget.linha == Sudeste.coordenada.linha)&&(Ctarget.coluna == Sudeste.coordenada.coluna)))
 	    			if (Sudeste.P == '-')
 	    				return true;
 	    		if (Sudeste.Sudeste != null)
-					if (((Ctarget.linha == coordenada.linha+2)&&(Ctarget.coluna == coordenada.coluna+2)))
-						if (Sudeste.P == 'b' || Sudeste.P == 'B')
+					if (((Ctarget.linha == Sudeste.Sudeste.coordenada.linha)&&(Ctarget.coluna == Sudeste.Sudeste.coordenada.coluna)))
+						if ((Sudeste.P == 'b' || Sudeste.P == 'B') && (Sudeste.Sudeste.P == '-'))
 							return true;
     		}
         	return false;
     	}
-    	else if (P == 'b') {
+    	else if (TipoSource == 'b') {
     		if (Noroeste != null)
-    			if (((Ctarget.linha == coordenada.linha-1)&&(Ctarget.coluna == coordenada.coluna-1)))
+    			if (((Ctarget.linha == Noroeste.coordenada.linha)&&(Ctarget.coluna == Noroeste.coordenada.coluna)))
     				if (Noroeste.P == '-')
     					return true;
     			if (Noroeste.Noroeste != null)
-    				if (((Ctarget.linha == coordenada.linha-2)&&(Ctarget.coluna == coordenada.coluna-2)))
+    				if (((Ctarget.linha == Noroeste.Noroeste.coordenada.linha)&&(Ctarget.coluna == Noroeste.Noroeste.coordenada.coluna)))
     	    			if ((Noroeste.P == 'b' || Noroeste.P == 'B') && (Noroeste.Noroeste.P == '-'))
     	    				return true;
     	    else if (Nordeste != null)
-    	    	if (((Ctarget.linha == coordenada.linha-1)&&(Ctarget.coluna == coordenada.coluna+1)))
+    	    	if (((Ctarget.linha == Nordeste.coordenada.linha)&&(Ctarget.coluna == Nordeste.coordenada.coluna)))
     	    		if (Nordeste.P == '-')
     	    			return true;
     			if (Nordeste.Nordeste != null)
-    				if (((Ctarget.linha == coordenada.linha-2)&&(Ctarget.coluna == coordenada.coluna+2)))
+    				if (((Ctarget.linha == Nordeste.Nordeste.coordenada.linha)&&(Ctarget.coluna == Nordeste.Nordeste.coordenada.coluna)))
     					if ((Nordeste.P == 'b' || Nordeste.P == 'B') && (Nordeste.Nordeste.P == '-'))
     						return true;
         	return false;
     	}
     	return false;
-    }
-    
-    public  boolean TestaMovimento(Coordenada Ctarget) {
-    	
-        return true;
     }
 }
--- src/mc322/lab05/PecaPeao.java
+++ src/mc322/lab05/PecaPeao.java
@@ -11,41 +11,6 @@ Coordenada coordenada = new Coordenada("a1");
     /*peças comuns se movem uma casa na diagonal para frente somente se a 
       posição estiver livre e for o seu lance;
     */
-	
-    
-    public boolean TestaPeca(Coordenada Ctarget) {
-    	if (P == 'p') {
-        	if (((Ctarget.linha == coordenada.linha+1)&&(Ctarget.coluna == coordenada.coluna-1)))
-        		if (Sudoeste.P == '-')
-        			return true;
-    		else if (((Ctarget.linha == coordenada.linha+1)&&(Ctarget.coluna == coordenada.coluna+1)))
-    			if (Sudeste.P == '-')
-    				return true;
-    		else if (((Ctarget.linha == coordenada.linha+2)&&(Ctarget.coluna == coordenada.coluna-2)))
-    			if (Sudoeste.P == 'b' || Sudoeste.P == 'B')
-    				return true;
-			else if (((Ctarget.linha == coordenada.linha+2)&&(Ctarget.coluna == coordenada.coluna+2)))
-    			if (Sudeste.P == 'b' || Sudeste.P == 'B')
-    				return true;
-        	return false;
-    	}
-    	else if (P == 'b') {
-    		if (((Ctarget.linha == coordenada.linha-1)&&(Ctarget.coluna == coordenada.coluna-1)))
-        		if (Noroeste.P == '-')
-        			return true;
-    		else if (((Ctarget.linha == coordenada.linha-1)&&(Ctarget.coluna == coordenada.coluna+1)))
-    			if (Nordeste.P == '-')
-    				return true;
-    		else if (((Ctarget.linha == coordenada.linha-2)&&(Ctarget.coluna == coordenada.coluna-2)))
-    			if (Noroeste.P == 'b' || Noroeste.P == 'B')
-    				return true;
-			else if (((Ctarget.linha == coordenada.linha-2)&&(Ctarget.coluna == coordenada.coluna+2)))
-    			if (Nordeste.P == 'b' || Nordeste.P == 'B')
-    				return true;
-        	return false;
-    	}
-    	return false;
-    }
     
     public  boolean TestaMovimento(Coordenada Ctarget) {
 
