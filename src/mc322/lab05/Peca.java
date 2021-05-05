ae727319a129ffb9c93d7962e5acd0a4d508aa88
Rakiakagi <henriqueakiraakagi@gmail.com>
2021-05-05 18:45:02 -0300

    no message

--- src/mc322/lab05/Peca.java
+++ src/mc322/lab05/Peca.java
@@ -35,4 +35,42 @@ public class Peca {
 		return "";
 
 	}
+	
+	public String leitura() {
+		String cond = "";
+		switch (coordenada.coluna) {
+		case 1:
+			cond = "a";
+			break;
+		case 2:
+			cond = "b";
+			break;
+		case 3:
+			cond = "c";
+			break;
+		case 4:
+			cond = "d";
+			break;
+		case 5:
+			cond = "e";
+			break;
+		case 6:
+			cond = "f";
+			break;
+		case 7:
+			cond = "g";
+			break;
+		case 8:
+			cond = "h";
+			break;
+		}
+		cond = cond + coordenada.linha;
+		if (P == '-')
+			cond = cond + "_";
+		else if (P == 'p'|| P =='P')
+			cond = cond + "p";
+		else
+			cond = cond + "b";
+		return cond;
+	}
 }
