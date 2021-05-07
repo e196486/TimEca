a2c5252aeb956201b9caad3d8b4c7fe57f431c2f
Rakiakagi <henriqueakiraakagi@gmail.com>
2021-05-07 13:58:58 -0300

    Atualizando Caverna e Sala

--- lab06/src/mc322/lab06/Sala.java
+++ lab06/src/mc322/lab06/Sala.java
@@ -1,5 +1,51 @@
 package mc322.lab06;
 
 public class Sala {
-
+	char P = '-';
+	Componente Wumpus = null,
+			Buraco = null,
+			Ouro = null,
+			Heroi = null,
+			Brisa = null,
+			Fedor = null;
+			
+	
+	
+	public boolean insereC(Componente C) {
+		if (Wumpus != null || Buraco != null || Ouro != null)// se já houver algum desses componentes na sala, retorna falso para a caverna.
+			return false;
+		else {
+			P = C.componente;
+			if (C.componente == 'W') {
+				Wumpus = C;
+				P = 'W';
+			}
+			else if (C.componente == 'B') {
+				Buraco = C;
+				P = 'B';
+			}
+			else if (C.componente == 'O') {
+				Ouro = C;
+				P = 'O';
+			}
+			else if (C.componente == 'P') {
+				Heroi = C;
+				if(Wumpus == null && Buraco == null && Ouro == null)
+					P = 'P';
+			}
+			else if (C.componente == 'f') {
+				Fedor = C;
+				if(Wumpus == null && Buraco == null && Ouro == null && Heroi == null)
+					P = 'f';
+			}else {
+				Brisa = C;
+				if(Wumpus == null && Buraco == null && Ouro == null && Heroi == null && Fedor == null)
+					P = 'b';
+			}
+			return true;
+		}
+	}
+	
+	
+	
 }
