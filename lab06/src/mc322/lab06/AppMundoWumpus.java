6f7e5816b03a1204ad467c54d98ab8b976daeeb0
Rakiakagi <henriqueakiraakagi@gmail.com>
2021-05-13 21:18:40 -0300

    no message

--- lab06/src/mc322/lab06/AppMundoWumpus.java
+++ lab06/src/mc322/lab06/AppMundoWumpus.java
@@ -3,7 +3,7 @@ package mc322.lab06;
 public class AppMundoWumpus {
 	
 	public static void main(String[] args) {
-		String Arquivo = "C:\\Users\\henri\\Documents\\teste\\Arq001.csv"; //criei um arquivo teste, ainda não adicionei na pasta.
+		String Arquivo = "C:\\Users\\henri\\Documents\\teste\\Arq001.csv";
 		MontadorCaverna M = new MontadorCaverna(Arquivo);
 		M.cave.retiraOuro(4, 3);
 		M.cave.imprimeEstado();
